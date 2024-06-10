7868d4b948c2b6974e05e863d2074b1381b1320e
Maria Mercedes Chomicki <mmchomicki1995@gmail.com>
2024-06-10 11:43:48 -0300

    Fixes

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package tpe;
 public class Main {
 
 	public static void main(String args[]) {
-		Servicios servicios = new Servicios("./src/main/java/tpe/datasets/Procesadores.csv", "./src/main/java/tpe/datasets/Tareas.csv");
+		Servicios servicios = new Servicios("datasets/Procesadores.csv", "datasets/Tareas.csv");
 		int tiempoMaxNoRefrigerado = 60;
 
 		//servicios.backtracking(tiempoMaxNoRefrigerado);
